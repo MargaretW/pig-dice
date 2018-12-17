afda1b6d7268a90ca81bace51dc361be44cf28a8
Margaret Wangechi <wangechimargaret7@gmail.com>
2018-12-17 08:08:03 +0300

    add styling and create more functions

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var pigDice= {
   playerTurn: 1,
   currentScore:0,
   rollDice:function() {
-    return math.ceil (math.random()* 6);
+    return math.ceil(math.random()* 6);
   }
 };
 
@@ -25,20 +25,28 @@ $(document).ready (function (){
    $("button#start").click (function(event){
     event.preventDefault ();
      $("#pigDice ").append('<div class="col-md-6"> ' +
-                            '<h2>First Player</h2>' +
-                            '<button type="button" id="player1Roll">ROLL</button>' +
-                            '<p id="rollDice1"> Die rolled is </p>' +
-                            '<p id= scoreOne> Your score is </p>' +
-                            '</div>' +
+                              '<h2>First Player</h2>' +
+                              '<button type="button" id="player1Roll">ROLL</button>' +
+                              '<div class="rollDice">' +
+                                '<p id="rollDice1"></p>' +
+                              '</div>' +
+                              '<div class="score">' +
+                                '<p id= scoreOne></p>' +
+                              '</div>' +
+                            '</div' +
                             '<div class="col-md-6">' +
-                            '<h2>Second Player</h2>' +
-                            '<button type="button" id="player2Roll">ROLL</button>' +
-                            '<p id="rollDice2"> Die rolled is </p>'+
-                           '<p id= scoreTwo> Your score is </p>' +
-                           '</div>'+
-                           '<button type="button" id="newGame"><a href="index.html">NewGame</a></button>'});
+                              '<h2>Second Player</h2>' +
+                              '<button type="button" id="player2Roll">ROLL</button>' +
+                              '<div class="rollDice">'+
+                                '<p id="rollDice2"></p>'+
+                              '</div>'+
+                              '<div class="score">' +
+                                '<p id= scoreTwo></p>' +
+                            '</div>'+
+                              '<button type="button" id="newGame"><a href="index.html">NewGame</a></button>');
+});
 
-  $("button#player1Roll"). click(function(e){
+  $("button#player1Roll").click(function(e){
 
     e.preventDefault ();
 
